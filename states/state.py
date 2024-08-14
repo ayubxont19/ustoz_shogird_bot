f6da3780b2b23b2f7812c2593357551d56bf6f7e
Ayubxon <ayubxont19@gmail.com>
2024-08-14 11:55:06 +0300

    first commit

---
+++ states/state.py
@@ -0,0 +1,17 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class ArizaOlish(StatesGroup):
+  FISH = State()
+  Yosh = State()
+  Texno = State()
+  Nomer = State()
+  Manzil = State()
+  Masulismsharifi = State()
+  Narxi = State()
+  Kasb = State()
+  Murojatvaqt = State()
+  Ishvaqt = State()
+  Maosh = State()
+  Maqsad = State()
+  Qoshimcha = State()
+  Idora = State()
